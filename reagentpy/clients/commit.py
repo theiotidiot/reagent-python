78a13bc4b85a0dbcd7573e62dee891125390dfec
Bibaud <alice@margin.re>
2024-12-10 17:12:34 -0500

    little demo fixes + commit method

--- reagentpy/clients/commit.py
+++ reagentpy/clients/commit.py
@@ -3,3 +3,19 @@ from reagentpy.clients import ReagentClient, ReagentResponse
 class CommitClient(ReagentClient):
     def __init__(self):
         super().__init__()
+        
+    def data(self, repo: str | None = None, limit: int = 50, email: str | None = None,
+            timezone: float | None = None, start_date: str | None = None, end_date: str | None = None, debug: bool = False):
+        """Given an email or repo name, get time info on all the commits a user or set of users has committed."""
+
+        query_params = {
+            "repo": repo,
+            "limit": limit,
+            "email": email,
+            "timezone": timezone,
+            "start_date": start_date,
+            "end_date": end_date,
+            "debug": debug
+        }
+
+        return ReagentResponse(self.session.get(f"{self.reagent_base_url}/commit/data", params=query_params))
