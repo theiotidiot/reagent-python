376755c8e200c95f40cec4881b579fb4b1b126a8
Bibaud <alice@margin.re>
2024-12-10 09:57:16 -0500

    undid unecessary change to init

--- reagentpy/__init__.py
+++ reagentpy/__init__.py
@@ -1,3 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+from reagentpy.constants import REAGENTPY_USER_AGENT
+
 from reagentpy.clients.community import CommunityClient
 from reagentpy.clients.repo import RepoClient
 from reagentpy.clients.user import UserClient
